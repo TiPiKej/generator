232c8dcfcdbb04b25a87f083f7b2401c9be3f004
TiPiKej <40639418+TiPiKej@users.noreply.github.com>
2018-07-29 21:36:42 +0200

    1.3
    copy to clipboard options added and changed styles

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ export default class App extends Component {
     super();
 
     this.state = {
+      limit: {
+        min: 1,
+        max: 36
+      },
       counts: 8,
       password: '',
       special: true,
@@ -25,25 +29,70 @@ export default class App extends Component {
   }
 
   changingCounts = ({currentTarget}, typ) => {
-    switch(currentTarget.type){
-      case 'text':
-        const value = currentTarget.value;
-        if(value < 6){
-          console.log('dsas')
-          console.log(currentTarget.validationMessage);
+    let counts = this.state.counts;
+    const limit = this.state.limit;
+    switch(typ){
+      case "up":
+        counts++;
+        /* turning on / off buttons */
+        if(limit.max === counts){
+          currentTarget.className += ' disable';
         }
+        currentTarget.previousSibling.previousSibling.className = 'changingValue';
 
-        this.setState({
-          counts: ((value >= 1) && (value <= 36))? value : this.state.counts
-        });
+        /* changing value of text input */
+        if(limit.max >= counts) this.setState({counts});
+        break;
+
+      case "down":
+        counts--;
+        
+        /* turning on / off buttons */
+        if(limit.min === counts){
+          currentTarget.className += ' disable';
+        }
+        currentTarget.nextSibling.nextSibling.className = 'changingValue';
+        /* changing value of text input */
+        if(limit.min <= counts) this.setState({counts});
         break;
 
       default:
+        switch(currentTarget.type){
+          case 'text':
+            const value = currentTarget.value;
 
-        this.setState({
-          [typ]: this.state[typ]? false: true
-        })
+            /* changing length of password */
+            let counts = this.state.counts;
+            if((value >= limit.min) && (value <= limit.max)) counts = value;
+            else if( value < limit.min ) counts = limit.min;
+            else if( value > limit.max ) counts = limit.max;
 
+
+            /* turning on / off buttons */
+            
+            currentTarget.previousSibling.className = 'changingValue';
+            currentTarget.nextSibling.className = 'changingValue';
+
+            if(Number(counts) === Number(limit.min)){
+              currentTarget.previousSibling.className += ' disable';
+            }
+            else if(Number(counts) === Number(limit.max)){
+              currentTarget.nextSibling.className += ' disable';
+            }
+
+
+            this.setState({ counts });
+            
+            break;
+
+          default:
+
+            this.setState({
+              [typ]: this.state[typ]? false: true
+            })
+
+            break;
+        }
         break;
     }
   }
@@ -64,6 +113,29 @@ export default class App extends Component {
       password += collection[random] === undefined? "Brak Danych " : collection[random];
     }
     this.setState({password});
+  }
+
+  clipboard = ({currentTarget}) => {
+    let children = currentTarget.parentNode.children,
+        textarea = Array.from(children).filter(el => el.type === 'textarea'),
+        pass;
+        Array.from(textarea).forEach(el => {
+          pass = el.value;
+          el.select();
+          document.execCommand('copy');
+        });
+
+
+    const tooltip = document.createElement('div');
+          tooltip.className = 'tooltip';
+          tooltip.innerHTML = `Skopiono do chowka: ${pass}`;
+          setTimeout(() => tooltip.style.opacity = 1, 10);
+          setTimeout(() => {
+            tooltip.style.opacity = 0,
+            setTimeout(() => tooltip.remove(), 500);
+          }, 3000);
+
+    currentTarget.appendChild(tooltip)
   }
 
   render() {
@@ -93,13 +165,16 @@ export default class App extends Component {
             checkedArray={this.state.letters}
             changingCounts={this.changingCounts}/>
           
-          <button onClick={this.password}>
+          <button 
+            className="passButton"
+            onClick={this.password}>
             Generuj hasło
           </button>
         </div>
 
         <Password  
-          password={this.state.password}/>
+          password={this.state.password}
+          copy={this.clipboard}/>
       </div>
     );
   }
