e831cd7b59ca7e990fa9ed311b279662eb44d290
TiPiKej <40639418+TiPiKej@users.noreply.github.com>
2018-07-29 14:24:13 +0200

    1.2

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ export default class App extends Component {
   }
 
   changingCounts = ({currentTarget}, typ) => {
-    console.log(currentTarget)
     switch(currentTarget.type){
       case 'text':
         const value = currentTarget.value;
