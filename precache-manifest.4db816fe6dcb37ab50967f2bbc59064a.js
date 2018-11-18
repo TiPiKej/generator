907f6aaab49ba65670565aa9a37ca8c2e239e075
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2018-11-18 17:02:06 +0100

    Updates

---
+++ precache-manifest.4db816fe6dcb37ab50967f2bbc59064a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "681f1403a530e36d52c6",
+    "url": "/generator/static/css/main.531a28bd.chunk.css"
+  },
+  {
+    "revision": "681f1403a530e36d52c6",
+    "url": "/generator/static/js/main.681f1403.chunk.js"
+  },
+  {
+    "revision": "164580a8dcfc8f64f334",
+    "url": "/generator/static/js/1.164580a8.chunk.js"
+  },
+  {
+    "revision": "3046095595ec3aae1148",
+    "url": "/generator/static/js/runtime~main.30460955.js"
+  },
+  {
+    "revision": "9cdd314c04ccfae1a54919fd755a1a51",
+    "url": "/generator/index.html"
+  }
+];
